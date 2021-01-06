e9ffff54cf1224b8e1ad6f08d0101a18a0e63de1
u-Hoshi <yuta000707@yahoo.co.jp>
2021-01-06 10:31:37 +0900

    一旦完成

---
+++ index.js
@@ -0,0 +1,9 @@
+function onclickbutton() {
+    var hoge = Math.random()*10
+    if (6 < hoge) {
+        alert("斎藤さんだぞぉぉぉぉぉ")
+    } else if (3 < hoge < 6){
+        alert("ぺっぺぺー")
+    }
+    console.log(hoge)
+}
